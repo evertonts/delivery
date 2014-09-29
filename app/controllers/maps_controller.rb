4ebb43576b4d4b0ac83168f4617bf929be4b80d1
Everton Silva <evertonsilva@Evertons-MacBook-Pro.local>
2014-09-28 21:03:16 -0300

    Add resouce to create new map

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,13 +1,23 @@
-class RoutesController < ApplicationController
-  def create  
-  end
+class MapsController < ApplicationController
+  rescue_from ActionController::ParameterMissing, with: :parameters_missing
   
-  def create_list
+  def create
+    map = Map.new(map_params)
+    if map.valid?
+      map.save
+      head :created
+    else
+      render json: { message: 'cannot create a map', cause: map.errors }, status: :unprocessable_entity
+    end
   end
   
   private
   
-  def routes_params
-    params.require(map: [])
+  def map_params
+    params.require(:map).permit(:name, routes: [])
+  end
+  
+  def parameters_missing
+    render json: { message: 'Invalid parameters' }, status: :bad_request
   end
 end
