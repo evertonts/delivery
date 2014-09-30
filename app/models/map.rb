d6424788be05b6c32b7795c1e0deeab87a58d54d
Everton Silva <everton.silva@bidu.com.br>
2014-09-30 18:01:08 -0300

    Start calculations for shortest path

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,5 +1,15 @@
 class Map < ActiveRecord::Base
   has_many :routes, autosave: true
-  
+
   validates :name, presence: true, uniqueness: true
+
+  scope :with_source, ->(source) { Map.joins(:routes).where(source: source) }
+  scope :with_destination, ->(destination) { Map.joins(:routes).where(source: destination) }
+
+  def self.shortest_path(source, destination)
+    Map.with_source.with_destination.each do |map|
+      routes = Route.from_map(map)
+
+    end
+  end
 end
--- app/models/route.rb
+++ app/models/route.rb
@@ -1,5 +1,7 @@
 class Route < ActiveRecord::Base
   belongs_to :map
-  
+
   validates :source, :destination, :distance, presence: true
+
+  scope :from_map, ->(map) { where(map: map) }
 end
